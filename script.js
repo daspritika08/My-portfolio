f1b2e4710a1135fd36b0bfb161e6d80c5276d749
Pritika Das <daspritika08@gmail.com>
2026-02-27 12:52:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const manuIcon = document.querySelector('#menu-icon');
-const navLinks= document.querySelector('.nav-links');
-
-menuLcon.onclick = () => {
-    navLinks.classList.toggle('active');
-}
